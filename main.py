64a1f6b68711e328011cbec040ee29fbebb1c563
Saulo Sena <saulobdsena@gmail.com>
2026-06-21 09:42:26 -0300

    feat(add_task, view_task): add methods add task and view task

--- main.py
+++ main.py
@@ -10,9 +10,27 @@ im treining my english just that
 
 task_list = []
 
+#Method to add new task in the task_list
+def add_task (task_list, task):
+    tasks = {"task": task, "completed": False}
+    task_list.append(task)
+    print(f"Task: {task} has been added")
+    return
+
+#Method to view the task list
+def view_tasks (task_list):
+    
+    for task in task_list:
+        print(f"[] {task}")
+
+    return
+
+
+
+
 while True:
 
-    print("Task List Menager Menu:")
+    print("\nTask List Menager Menu:")
     print("1. Add Task")
     print("2. Show the task list")
     print("3. Update task")
@@ -20,20 +38,17 @@ while True:
     print("5. Delete a completed task")
     print("6. End the program")
 
-    response =  int(input("Enter your choise  "))
+    response =  int(input("Enter your choise "))
 
     match response:
 
         #Case to add new task to the list 
         case 1:
-            new_task = input("Digite a tarefa: ")           
-            task_list.append(new_task)
-            print(f"Task added: {new_task}")
-
+            new_task = input("Enter the task: ")           
+            add_task(task_list, new_task)
         #Case to show the task list
         case 2:
-            print(task_list)
-
+            view_tasks(task_list)
         #Case to update the task
         case 3:
             print()
